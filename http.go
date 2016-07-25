1162e3207ded5d9df399d211aea2ce72c5389b00
Kelsey Hightower <kelsey.hightower@gmail.com>
2016-07-24 21:49:23 -0700

    document the usage of the Certifi CA bundle

--- http.go
+++ http.go
@@ -22,6 +22,9 @@ import (
 var httpClient http.Client
 
 func init() {
+	// Use the Root Certificates bundle from the Certifi project so we don't
+	// rely on the host OS or container base images for a CA Bundle.
+	// See https://certifi.io for more details.
 	certPool, err := gocertifi.CACerts()
 	if err != nil {
 		log.Fatal(err)
